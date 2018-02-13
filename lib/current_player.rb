d085b9e325794a8444498b67124f1ccfd3b9ca35
Antonio Troncoso <antonio.troncoso@wework.com>
2018-02-13 04:47:51 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  i = 0
+  board.each do |turn|
+    if turn != " " && turn != ""
+      i = i + 1
+    end
+  end
+  i
+end
+
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+#    "X"
+#  else
+#    "O"
+#  end
+#end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
